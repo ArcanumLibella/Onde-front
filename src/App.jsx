77b2e44651ee386170b8d4b762655d6996c23c99
KalaniM <kalakalanicontact@gmail.com>
2020-06-19 22:54:02 +0200

    add logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App () {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>ONDE</p>
-        <p>Que c'est beau !!!</p>
-      </header>
-    </div>
-  )
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>Que c'est beau !!!</p>
+			</header>
+		</div>
+	);
 }
 
-export default App
+export default App;
