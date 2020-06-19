fc825c9562c4dacd4bb013cae121fc3c401b4bd5
KalaniM <kalakalanicontact@gmail.com>
2020-06-19 23:02:37 +0200

    create router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
 	return (
@@ -9,8 +10,51 @@ function App() {
 				<img src={logo} className='App-logo' alt='logo' />
 				<p>Que c'est beau !!!</p>
 			</header>
+			<Router>
+				<div>
+					<nav>
+						<ul>
+							<li>
+								<Link to='/'>Home</Link>
+							</li>
+							<li>
+								<Link to='/about'>About</Link>
+							</li>
+							<li>
+								<Link to='/users'>Users</Link>
+							</li>
+						</ul>
+					</nav>
+
+					{/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+					<Switch>
+						<Route path='/about'>
+							<About />
+						</Route>
+						<Route path='/users'>
+							<Users />
+						</Route>
+						<Route path='/'>
+							<Home />
+						</Route>
+					</Switch>
+				</div>
+			</Router>
 		</div>
 	);
+}
+
+function Home() {
+	return <h2>Home</h2>;
+}
+
+function About() {
+	return <h2>About</h2>;
+}
+
+function Users() {
+	return <h2>Users</h2>;
 }
 
 export default App;
