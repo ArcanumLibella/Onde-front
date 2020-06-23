207d460efba188d00323764ae2441ca4c371ad5d
KalaniM <43987061+KalaniM@users.noreply.github.com>
2020-06-23 09:53:52 +0200

    add pages and route them (#3)

---
+++ src/pages/Homepage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Homepage extends React.Component {
+	render() {
+		return <h1>Apporter sa goutte à la mer !</h1>;
+	}
+}
+
+export default Homepage;
---
+++ src/pages/Missions.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return <h1>Apporter sa goutte à la mer !</h1>;
+  }
+}
+
+export default Missions;
