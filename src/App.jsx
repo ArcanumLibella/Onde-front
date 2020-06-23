8f2328c06424626d63c8cee27c284730107bf8c3
Fiona Roux <43987001+ArcanumLibella@users.noreply.github.com>
2020-06-23 18:18:43 +0200

    Develop (#11)
    * :art: Create structure of menu component
    
    * :sparkles: Create navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
+/* eslint-disable import/no-webpack-loader-syntax */
 import React from 'react';
 import './assets/scss/app.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Homepage, Initiatives, Connexion, Missions } from './pages';
+import { ThemeProvider } from 'styled-components';
 
 // COMPONENTS
-import Menu from './components/Menu';
+import Navigation from './components/Navigation';
+
+// Extract our Sass variables into a JS object
+const theme = require('sass-extract-loader?{"plugins": ["sass-extract-js"]}!./assets/scss/global/_variables.scss');
 
 function App() {
-  return (
-    <div className="app">
-      <Router>
-        <Menu />
-        <div>
-          <Switch>
-            <Route path="/initiatives">
-              <Initiatives />
-            </Route>
-            <Route path="/connexion">
-              <Connexion />
-            </Route>
-            <Route path="/missions">
-              <Missions />
-            </Route>
-            <Route path="/">
-              <Homepage />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<div className="app">
+				<Router>
+					<Navigation />
+					<div>
+						<Switch>
+							<Route path="/initiatives">
+								<Initiatives />
+							</Route>
+							<Route path="/connexion">
+								<Connexion />
+							</Route>
+							<Route path="/missions">
+								<Missions />
+							</Route>
+							<Route path="/">
+								<Homepage />
+							</Route>
+						</Switch>
+					</div>
+				</Router>
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default App;
