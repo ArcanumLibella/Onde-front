2a3d632b43d94167396989a1b941a32ad6ec3ca8
ArcanumLibella <fiona.roux@hetic.net>
2020-06-23 11:04:46 +0200

    :art: Create structure of menu component

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,14 +1,9 @@
 import React from 'react';
 
 class Missions extends React.Component {
-  render() {
-    return (
-      <h1>
-        Une plateforme qui encourage la prise de parole et les échanges entre
-        activistes
-      </h1>
-    );
-  }
+	render() {
+		return <h2>Une plateforme qui encourage la prise de parole et les échanges entre activistes</h2>;
+	}
 }
 
 export default Missions;
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Homepage from './Homepage';
+import Connexion from './Connexion';
+import Initiatives from './Initiatives';
+import Missions from './Missions';
+
+export { Homepage, Connexion, Initiatives, Missions };
