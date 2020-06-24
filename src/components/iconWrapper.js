35b38269a63d234f781c21beff1c40c4c8fe17be
Fiona Roux <43987001+ArcanumLibella@users.noreply.github.com>
2020-06-24 16:06:56 +0200

    :sparkles: Add color and style header (#15)

--- src/components/iconWrapper.js
+++ src/components/iconWrapper.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { assets } from './index';
 
-const IconWrapper = props => {
-  const { name } = props;
+const IconWrapper = (props) => {
+	const { name } = props;
 
-  if (!assets[name]) {
-    return null;
-  }
+	if (!assets[name]) {
+		return null;
+	}
 
-  return <img alt="img" src={assets[name]} />;
+	return <img alt={name} src={assets[name]} />;
 };
 
 export default IconWrapper;
