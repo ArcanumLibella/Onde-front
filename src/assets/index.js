74d1e5b242664675d2da7b4dbfef011e101039c1
Fiona Roux <43987001+ArcanumLibella@users.noreply.github.com>
2020-06-25 16:22:35 +0200

    Feature navigation (#17)
    * :sparkles: Add color and style header
    
    * fix: merge
    
    * :sparkles: Add SVG-inliner lib and update tabpar with icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,6 @@
 // icons
-export { default as HomeFilled } from './icons/home-filled.svg';
-export { default as HomeOutlined } from './icons/home-filled.svg';
+export { default as HomeF } from './icons/home-f.svg';
+export { default as HomeO } from './icons/home-o.svg';
 
 // images
 export { default as OndeLogo } from './images/logo.svg';
