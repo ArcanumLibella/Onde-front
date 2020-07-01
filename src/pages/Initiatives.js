538f12b611ba718c8d307b79f3dd7c23beb973c0
KalaniM <kalakalanicontact@gmail.com>
2020-07-01 09:57:13 +0200

    manipulate data

--- src/pages/Initiatives.js
+++ src/pages/Initiatives.js
@@ -13,62 +13,28 @@ const Initiatives = props => {
   useEffect(() => {
     fetch('http://18.224.138.171/api/posts')
       .then(res => res.json())
-      .then(
-        result => {
-          setIsLoaded(true);
-          setItems(result.items);
-        },
-        error => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
+      .then(result => {
+        setIsLoaded(true);
+        setItems(result);
+      })
+      .catch(error => {
+        setIsLoaded(true);
+        setError(error);
+      });
   }, []);
 
-  console.log('items', items);
-  console.log('isLoaded', isLoaded);
-  console.log('error', error);
+  const list = items['hydra:member'];
 
-  // const list = [
-  //   {
-  //     id: 1,
-  //     name: 'Nettoyer la plage de Pampelonne',
-  //     description:
-  //       'La plage de Pampelonne accueille chaque été de nombreux fêtards avec comme conséquence des déchets qui s’accumulent. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.',
-  //     likes: 20,
-  //     tags: [1, 2]
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'Végétaliser le parking de la plage de l’Espiguette',
-  //     description:
-  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.',
-  //     likes: 12,
-  //     tags: [3]
-  //   },
-  //   {
-  //     id: 3,
-  //     name: 'Mobilisation contre la pêche des oursins',
-  //     description:
-  //       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.',
-  //     likes: 203,
-  //     tags: [1, 4]
-  //   }
-  // ];
-
-  const initiativesCollection = items.map(i => (
-    <InitiativesCard key={i} theme={theme} initiative={i} />
-  ));
-
-  // const coll = [1, 2, 3, 4, 5];
-  // const listItems = coll.map(i => <InitiativesCard key={i} theme={theme} />);
+  const initiativesCollection =
+    list &&
+    list.map(i => <InitiativesCard key={i.id} theme={theme} initiative={i} />);
 
   return (
     <InitiativesStyled>
-      <DisplayTitle>Vos initiatives</DisplayTitle>
+      <DisplayTitle as="p">Vos initiatives</DisplayTitle>
       <ParametersCircleF width="40" fill={theme.midnight} />
       <Tag />
-      {initiativesCollection}
+      {isLoaded || error ? initiativesCollection : 'wait...'}
     </InitiativesStyled>
   );
 };
