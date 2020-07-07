b9ead413bc622d2b680dbb4c9913c057543658db
ArcanumLibella <fiona.roux@hetic.net>
2020-07-07 18:14:30 +0200

    FEATURE: Style form initiative creation on mobile

--- src/pages/InitiativeCreate.js
+++ src/pages/InitiativeCreate.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-import { ApiService, DisplayTitle, InitiativesCard, Tag } from '../components';
-import { ParametersCircleF } from '../assets';
-import { devices } from '../utilities';
 import Axios from 'axios';
+import { DisplayTitle, InputText } from '../components';
+// import { ParametersCircleF } from '../assets';
+import { devices, rem } from '../utilities';
 
 const InitiativeCreate = (props) => {
 	const [ tags, setTags ] = useState([]);
@@ -81,57 +81,167 @@ const InitiativeCreate = (props) => {
 	};
 
 	return (
-		<div>
-			<form
-				onSubmit={(e) => {
-					createInitiative(e);
-				}}
-			>
-				<label>Nom de l'initiative</label>
-				<select>
-					{tags.map((tag) => {
-						return (
-							<option key={tag.id} value={tag.id}>
-								{tag.name}
-							</option>
-						);
-					})}
-				</select>
+		<InitiativeCreateStyled className="initiativeCreation">
+			<div className="content-wrapper">
+				<DisplayTitle /* tag="h1" */>Créer votre initiative</DisplayTitle>
+				<form
+					onSubmit={(e) => {
+						createInitiative(e);
+					}}
+					className="form"
+				>
+					<fieldset className="form__field">
+						<label>
+							<InputText>Thème</InputText>
+						</label>
+						<select>
+							{tags.map((tag) => {
+								return (
+									<option key={tag.id} value={tag.id}>
+										{tag.name}
+									</option>
+								);
+							})}
+						</select>
+					</fieldset>
 
-				<label>Nom de l'initiative</label>
-				<input type="text" name="name" />
+					<fieldset className="form__field">
+						<label>
+							<InputText>Titre de l'initiative</InputText>
+						</label>
+						<input type="text" name="name" />
+					</fieldset>
 
-				<label>Description</label>
-				<textarea name="description" />
+					<fieldset className="form__field">
+						<label>
+							<InputText>Description</InputText>
+						</label>
+						<textarea name="description" />
+					</fieldset>
 
-				<label>Emplacement</label>
-				<select name="department">
-					{departements.map((department) => {
-						return (
-							<option key={department.id} value={department.id}>
-								{department.name}
-							</option>
-						);
-					})}
-				</select>
+					<fieldset className="form__field">
+						<label>
+							<InputText>Emplacement</InputText>
+						</label>
+						<select name="department">
+							{departements.map((department) => {
+								return (
+									<option key={department.id} value={department.id}>
+										{department.name}
+									</option>
+								);
+							})}
+						</select>
+					</fieldset>
 
-				<div>
-					<label>Objectifs</label>
-					<input type="number" name="number[]" />
-					<label>Nom de l'objectif</label>
-					<input type="text" name="nameObjectif[]" />
-				</div>
+					<div className="form__fields">
+						<fieldset className="form__field">
+							<label>
+								<InputText>Objectif</InputText>
+							</label>
+							<input type="number" name="number[]" />
+						</fieldset>
+						<fieldset className="form__field">
+							<label>
+								<InputText>Titre de l'objectif</InputText>
+							</label>
+							<input type="text" name="nameObjectif[]" />
+						</fieldset>
+					</div>
 
-				<label>Date et horraire du rendez-vous</label>
-				<input type="datetime-local" name="dateMeeting" />
+					<fieldset className="form__field">
+						<label>
+							<InputText>Date et horaire du rendez-vous</InputText>
+						</label>
+						<input type="datetime-local" name="dateMeeting" />
+					</fieldset>
 
-				<label>Date de fin</label>
-				<input type="date" name="dateEnd" />
+					<fieldset className="form__field">
+						<label>
+							<InputText>Date de fin</InputText>
+						</label>
+						<input type="date" name="dateEnd" />
+					</fieldset>
 
-				<input type="submit" name="Voir l'aperçu" />
-			</form>
-		</div>
+					<input className="form__button" type="submit" name="Voir l'aperçu" placeholder="Voir l'aperçu" />
+				</form>
+			</div>
+		</InitiativeCreateStyled>
 	);
 };
+
+// STYLE
+const InitiativeCreateStyled = styled.main`
+	&.initiativeCreation {
+		padding-bottom: 80px;
+		height: 100%;
+
+		h2 {
+			margin-bottom: 24px;
+		}
+
+		.form {
+			&__field {
+				margin: 16px 0;
+
+				input,
+				select,
+				textarea {
+					padding: 12px 16px;
+					width: 100%;
+					border: 1px solid #001e3c;
+					border-radius: 2px;
+					box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.12);
+				}
+
+				select {
+					cursor: pointer;
+				}
+			}
+
+			&__fields {
+				display: flex;
+
+				.form__field {
+					&:first-child {
+						margin-right: 16px;
+						width: 30%;
+					}
+					&:nth-child(2) {
+						margin-right: 0;
+						width: calc(70% - 16px);
+					}
+				}
+			}
+
+			&__button {
+				position: relative;
+				display: block;
+				padding: 14px 20px;
+				margin: 0 auto;
+				width: 100%;
+				max-width: 288px;
+
+				color: ${(props) => props.theme.white};
+				font-size: ${rem(17)};
+				font-weight: 500;
+				line-height: 22px;
+				letter-spacing: 0.3px;
+				background-color: ${(props) => props.backgroundColor || props.theme.malibuBlue};
+				border: none;
+				border-radius: 4px;
+				box-shadow: none;
+				cursor: pointer;
+				transition: all 0.3s ease-in-out;
+				z-index: 1;
+
+				&:hover {
+					background-color: ${(props) => props.backgroundColor || props.theme.darkBlue};
+					box-shadow: 0px 5px 10px $malibuBlue;
+				}
+			}
+		}
+	}
+`;
 
 export default InitiativeCreate;
