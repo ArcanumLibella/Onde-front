b677b44e16438d73607e0d8b5c0a8c9c7fc12b8e
Fiona Roux <43987001+ArcanumLibella@users.noreply.github.com>
2020-07-09 13:34:05 +0200

    FIX: Button link (#75)

--- src/components/InitiativesCardDetails.js
+++ src/components/InitiativesCardDetails.js
@@ -4,125 +4,112 @@ import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 
 import {
-  DisplayTitle,
-  Title,
-  GoalWrapper,
-  Paragraph,
-  Button,
-  MiniTag,
-  Comment,
-  TextLink,
-  DropButton
+	DisplayTitle,
+	Title,
+	GoalWrapper,
+	Paragraph,
+	Button,
+	MiniTag,
+	Comment,
+	TextLink,
+	DropButton
 } from '../components';
 import { DropO, ShareCircleF, BackO } from '../assets';
 import { devices, formatDate } from '../utilities';
 
-const InitiativesCardDetails = props => {
-  const { theme } = props;
-  const { id } = useParams();
+const InitiativesCardDetails = (props) => {
+	const { theme } = props;
+	const { id } = useParams();
 
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [initiative, setInitiative] = useState();
+	const [ isLoaded, setIsLoaded ] = useState(false);
+	const [ initiative, setInitiative ] = useState();
 
-  useEffect(() => {
-    !isLoaded &&
-      axios
-        .get(`https://onde-api.frb.io/api/posts/${id}`)
-        .then(result => {
-          setIsLoaded(true);
-          setInitiative(result);
-        })
-        .catch(error => {
-          setIsLoaded(true);
-        });
-  });
+	useEffect(() => {
+		!isLoaded &&
+			axios
+				.get(`https://onde-api.frb.io/api/posts/${id}`)
+				.then((result) => {
+					setIsLoaded(true);
+					setInitiative(result);
+				})
+				.catch((error) => {
+					setIsLoaded(true);
+				});
+	});
 
-  const destructureData = () => {
-    if (initiative) {
-      const {
-        data: { Post, Tags, Goal }
-      } = initiative;
-      console.log(Post);
+	const destructureData = () => {
+		if (initiative) {
+			const { data: { Post, Tags, Goal } } = initiative;
 
-      const {
-        id,
-        name,
-        description,
-        dateCreated,
-        dateEnd,
-        dateMeeting,
-        likes
-      } = Post;
+			const { id, name, description, dateCreated, dateEnd, dateMeeting, likes } = Post;
 
-      return (
-        <InitiativesCardDetailsStyled className="initiativeDetails">
-          <div className="content-wrapper">
-            <div className="initiativeDetails__head">
-              <div className="initiativeDetails__title">
-                <DisplayTitle>Vos initiatives</DisplayTitle>
-                <Paragraph fontWeight={500} color={theme.blue}>
-                  Sur le littoral méditerranéen
-                </Paragraph>
-              </div>
-              <Link to={`/initiatives`}>
-                <div className="initiativeDetails__back">
-                  <BackO width={24} />
-                  <TextLink color={theme.black}>Retour</TextLink>
-                </div>
-              </Link>
-            </div>
-            <div className="initiativeDetails__header">
-              <MiniTag tags={Tags} />
-              <div className="initiativeDetails__icons">
-                <ShareCircleF width="34" fill={theme.midnight} />
-                <DropButton width="34" fill={theme.blue} initiativeId={id} />
-              </div>
-            </div>
-            <div className="initiativeDetails__content">
-              <DisplayTitle>{name}</DisplayTitle>
-              <Paragraph>{description}</Paragraph>
-              <div className="initiativeDetails__cta">
-                <Paragraph>
-                  <em>Germain Langelier</em> a lancé cette initiative le
-                  <time> {formatDate(dateCreated)}</time>.
-                </Paragraph>
-                <Button textCta="Je participe" linkCta={`/initiatives/${id}`} />
-              </div>
-            </div>
-            <div className="initiativeDetails__infos">
-              <div className="initiativeDetails__action">
-                <DropO width="34" />
-                <Paragraph>
-                  {amountOf(likes)}
-                  {amountOf(likes) > 1
-                    ? ' personnes ont aimé'
-                    : ' personne a aimé'}
-                </Paragraph>
-              </div>
-              {/* <div className="initiativeDetails__action">
+			return (
+				<InitiativesCardDetailsStyled className="initiativeDetails">
+					<div className="content-wrapper">
+						<div className="initiativeDetails__head">
+							<div className="initiativeDetails__title">
+								<DisplayTitle>Vos initiatives</DisplayTitle>
+								<Paragraph fontWeight={500} color={theme.blue}>
+									Sur le littoral méditerranéen
+								</Paragraph>
+							</div>
+							<Link to={`/initiatives`}>
+								<div className="initiativeDetails__back">
+									<BackO width={24} />
+									<TextLink color={theme.black}>Retour</TextLink>
+								</div>
+							</Link>
+						</div>
+						<div className="initiativeDetails__header">
+							<MiniTag tags={Tags} />
+							<div className="initiativeDetails__icons">
+								<ShareCircleF width="34" fill={theme.midnight} />
+								<DropButton width="34" fill={theme.blue} initiativeId={id} />
+							</div>
+						</div>
+						<div className="initiativeDetails__content">
+							<DisplayTitle>{name}</DisplayTitle>
+							<Paragraph>{description}</Paragraph>
+							<div className="initiativeDetails__cta">
+								<Paragraph>
+									<em>Germain Langelier</em> a lancé cette initiative le
+									<time> {formatDate(dateCreated)}</time>.
+								</Paragraph>
+								<Button textCta="Je participe" linkCta={`/initiatives/${id}`} />
+							</div>
+						</div>
+						<div className="initiativeDetails__infos">
+							<div className="initiativeDetails__action">
+								<DropO width="34" />
+								<Paragraph>
+									{amountOf(likes)}
+									{amountOf(likes) > 1 ? ' personnes ont aimé' : ' personne a aimé'}
+								</Paragraph>
+							</div>
+							{/* <div className="initiativeDetails__action">
 								<FistRaisedF width="34" />
 								<Paragraph>
                   {amountOf(subscriptions)} personnes participent
                 </Paragraph>
 							</div> */}
-            </div>
-            <div className="initiativeDetails__infos">
-              {/* {objectifs &&  */}
-              <Title>Prochains objectifs</Title>
-              <GoalWrapper goals={Goal} />
-            </div>
-            <div className="initiativeDetails__dates">
-              <div className="initiativeDetails__infos">
-                <Title>Date de fin</Title>
-                <Paragraph>{formatDate(dateEnd)}</Paragraph>
-              </div>
-              <div className="initiativeDetails__infos">
-                <Title>Rendez-vous</Title>
-                <Paragraph>{formatDate(dateMeeting)}</Paragraph>
-                <Paragraph>Plage de Pampelonne</Paragraph>
-              </div>
-            </div>
-            {/* <div className="initiativeDetails__infos">
+						</div>
+						<div className="initiativeDetails__infos">
+							{/* {objectifs &&  */}
+							<Title>Prochains objectifs</Title>
+							<GoalWrapper goals={Goal} />
+						</div>
+						<div className="initiativeDetails__dates">
+							<div className="initiativeDetails__infos">
+								<Title>Date de fin</Title>
+								<Paragraph>{formatDate(dateEnd)}</Paragraph>
+							</div>
+							<div className="initiativeDetails__infos">
+								<Title>Rendez-vous</Title>
+								<Paragraph>{formatDate(dateMeeting)}</Paragraph>
+								<Paragraph>Plage de Pampelonne</Paragraph>
+							</div>
+						</div>
+						{/* <div className="initiativeDetails__infos">
               <Title>Emplacement</Title>
               <iframe
                 title="map"
@@ -132,185 +119,185 @@ const InitiativesCardDetails = props => {
                 allowFullScreen
               />
             </div> */}
-            <div className="initiativeDetails__infos">
-              <Title>Commentaires</Title>
-              <Comment
-                author="Julie Aubé"
-                comment="Belle initiative ! Il est temps d’agir pour préserver la beauté de nos plages."
-                likeNumber="7"
-                theme={theme}
-              />
-              <Comment
-                author="Lucas Beaulac"
-                comment="Aucune mesure d’interdiction n’est prise pour les fêtes sauvages organisées sur la plage. Résultat, depuis quelques années celle-ci ressemble à une décharge !
+						<div className="initiativeDetails__infos">
+							<Title>Commentaires</Title>
+							<Comment
+								author="Julie Aubé"
+								comment="Belle initiative ! Il est temps d’agir pour préserver la beauté de nos plages."
+								likeNumber="7"
+								theme={theme}
+							/>
+							<Comment
+								author="Lucas Beaulac"
+								comment="Aucune mesure d’interdiction n’est prise pour les fêtes sauvages organisées sur la plage. Résultat, depuis quelques années celle-ci ressemble à une décharge !
           Honteux !"
-                likeNumber="35"
-                theme={theme}
-              />
-            </div>
-          </div>
-        </InitiativesCardDetailsStyled>
-      );
-    }
+								likeNumber="35"
+								theme={theme}
+							/>
+						</div>
+					</div>
+				</InitiativesCardDetailsStyled>
+			);
+		}
 
-    return 'Chargement des initiatives...';
-  };
+		return 'Chargement des initiatives...';
+	};
 
-  const amountOf = data => (data ? data.length : 0);
+	const amountOf = (data) => (data ? data.length : 0);
 
-  return destructureData();
+	return destructureData();
 };
 
 const InitiativesCardDetailsStyled = styled.main`
-  .initiativeDetails {
-    &__head {
-      display: flex;
-      justify-content: space-between;
-    }
+	.initiativeDetails {
+		&__head {
+			display: flex;
+			justify-content: space-between;
+		}
 
-    &__title {
-      h1,
-      h2 {
-        margin-bottom: 12px;
-      }
+		&__title {
+			h1,
+			h2 {
+				margin-bottom: 12px;
+			}
 
-      p {
-        margin-bottom: 40px;
-      }
-    }
+			p {
+				margin-bottom: 40px;
+			}
+		}
 
-    &__back {
-      display: flex;
-      align-items: center;
-      cursor: pointer;
+		&__back {
+			display: flex;
+			align-items: center;
+			cursor: pointer;
 
-      p {
-        margin-left: 8px;
-        text-decoration: none;
-      }
-    }
+			p {
+				margin-left: 8px;
+				text-decoration: none;
+			}
+		}
 
-    &__header {
-      display: flex;
-      align-items: flex-start;
-      justify-content: space-between;
-      margin-bottom: 12px;
+		&__header {
+			display: flex;
+			align-items: flex-start;
+			justify-content: space-between;
+			margin-bottom: 12px;
 
-      .minitags {
-        flex-wrap: wrap;
-      }
-    }
+			.minitags {
+				flex-wrap: wrap;
+			}
+		}
 
-    &__icons {
-      display: flex;
+		&__icons {
+			display: flex;
 
-      .icon {
-        margin: 0 4px;
-      }
-    }
+			.icon {
+				margin: 0 4px;
+			}
+		}
 
-    &__content {
-      h2,
-      p {
-        margin-bottom: 16px;
+		&__content {
+			h2,
+			p {
+				margin-bottom: 16px;
 
-        @media ${devices.large} {
-          margin-bottom: 24px;
-        }
-      }
+				@media ${devices.large} {
+					margin-bottom: 24px;
+				}
+			}
 
-      em,
-      time {
-        font-style: normal;
-        font-weight: bold;
-      }
+			em,
+			time {
+				font-style: normal;
+				font-weight: bold;
+			}
 
-      button {
-        margin-bottom: 24px;
-      }
-    }
+			button {
+				margin-bottom: 24px;
+			}
+		}
 
-    &__cta {
-      @media ${devices.medium} {
-        display: flex;
-        align-items: flex-end;
-        justify-content: space-between;
-        margin: 16px 0 32px;
+		&__cta {
+			@media ${devices.medium} {
+				display: flex;
+				align-items: flex-end;
+				justify-content: space-between;
+				margin: 16px 0 32px;
 
-        p,
-        button {
-          margin: 0;
-        }
-      }
-    }
+				p,
+				button {
+					margin: 0;
+				}
+			}
+		}
 
-    &__infos {
-      padding: 32px 0 24px;
-      border-top: 1px solid lightgrey;
+		&__infos {
+			padding: 32px 0 24px;
+			border-top: 1px solid lightgrey;
 
-      @media ${devices.large} {
-        padding: 64px 0 32px;
-      }
+			@media ${devices.large} {
+				padding: 64px 0 32px;
+			}
 
-      h3 {
-        margin-bottom: 24px;
-      }
+			h3 {
+				margin-bottom: 24px;
+			}
 
-      &:last-child {
-        margin-bottom: 64px;
+			&:last-child {
+				margin-bottom: 64px;
 
-        @media ${devices.large} {
-          margin-bottom: 0;
-        }
-      }
-    }
+				@media ${devices.large} {
+					margin-bottom: 0;
+				}
+			}
+		}
 
-    &__action {
-      display: flex;
-      align-items: center;
-      margin-bottom: 12px;
+		&__action {
+			display: flex;
+			align-items: center;
+			margin-bottom: 12px;
 
-      p {
-        margin-left: 4px;
-      }
-    }
+			p {
+				margin-left: 4px;
+			}
+		}
 
-    &__dates {
-      @media ${devices.large} {
-        display: flex;
-        flex-direction: row-reverse;
-        justify-content: space-between;
-        border-top: 1px solid lightgrey;
+		&__dates {
+			@media ${devices.large} {
+				display: flex;
+				flex-direction: row-reverse;
+				justify-content: space-between;
+				border-top: 1px solid lightgrey;
 
-        .initiativeDetails__infos {
-          border: none;
+				.initiativeDetails__infos {
+					border: none;
 
-          &:last-child {
-            margin-bottom: 0;
-          }
-        }
-      }
+					&:last-child {
+						margin-bottom: 0;
+					}
+				}
+			}
 
-      .initiativeDetails__infos {
-        &:last-child {
-          margin-bottom: 0;
-        }
-      }
-    }
-  }
+			.initiativeDetails__infos {
+				&:last-child {
+					margin-bottom: 0;
+				}
+			}
+		}
+	}
 
-  iframe {
-    width: 100%;
-    height: 100%;
-  }
+	iframe {
+		width: 100%;
+		height: 100%;
+	}
 
-  .comment {
-    margin-bottom: 24px;
+	.comment {
+		margin-bottom: 24px;
 
-    > p {
-      margin-bottom: 8px;
-    }
-  }
+		> p {
+			margin-bottom: 8px;
+		}
+	}
 `;
 
 export default InitiativesCardDetails;
